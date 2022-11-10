ddcea331e34e8e9df38d4b46437dc87aa2f782d1
Luis Alberto Gómez <37640684+luisgr97@users.noreply.github.com>
2022-11-09 21:19:22 -0500

    feat: new secctions

--- js/index.js
+++ js/index.js
@@ -1,15 +1,14 @@
-$(function(){
-    var str = '#len'; //increment by 1 up to 1-nelemnts
-    $(document).ready(function(){
-      var i, stop;
-      i = 1;
-      stop = 4; //num elements
-      setInterval(function(){
-        if (i > stop){
-          return;
-        }
-        $('#len'+(i++)).toggleClass('bounce');
-      }, 500)
-    });
+$(document).ready(function() {
+  $(window).scroll(function(){
+          if(document.body.scrollTop > 300)
+                  $('#fixmenu').fadeIn( "slow", function() { });
+          else    
+                  $('#fixmenu').fadeOut( "slow", function() { });
   });
+
+  $('a#srolltotop').click(function(){
+          $('html, body').animate({scrollTop:0}, 100);
+          return false;
+  });
+});
   
